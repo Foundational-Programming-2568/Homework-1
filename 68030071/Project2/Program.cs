69e7b53e219ea0938459bb539af6a6a02dcfd6c8
titikarn071 <68030071@kmitl.ac.th>
2025-06-11 16:16:36 +0700

    ส่งงาน

---
+++ 68030071/Project2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+Console.Write("กรุณาใส่อายุของคุณ: ");
+string input = Console.ReadLine();
+int age;
+
+if (int.TryParse(input, out age))
+{
+    if (age >= 1 && age <= 12)
+    {
+        Console.WriteLine("เด็ก");
+    }
+    else if (age >= 13 && age <= 19)
+    {
+        Console.WriteLine("วัยรุ่น");
+    }
+    else if (age >= 20 && age <= 50)
+    {
+        Console.WriteLine("วัยผู้ใหญ่");
+    }
+    else if (age >= 51)
+    {
+        Console.WriteLine("วัยชรา");
+    }
+    else
+    {
+        Console.WriteLine("กรุณาใส่อายุที่ถูกต้อง");
+    }
+}
+else
+{
+    Console.WriteLine("กรุณาใส่ตัวเลขที่ถูกต้อง");
+}
