5bf5780edf5312c9c347c3fb309a6023dfccf836
Sorawitsit19 <68030288@kmitl.ac.th>
2025-06-11 16:26:28 +0700

    homework

---
+++ homework/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("กรุณาใส่อายุ: ");
+string? input = Console.ReadLine();
+
+if (int.TryParse(input, out int age))
+{
+    if (age >= 1 && age <= 12)
+    {
+        Console.WriteLine("คุณอยู่ในช่วงวัย: เด็ก");
+    }
+    else if (age >= 13 && age <= 19)
+    {
+        Console.WriteLine("คุณอยู่ในช่วงวัย: วัยรุ่น");
+    }
+    else if (age >= 20 && age <= 50)
+    {
+        Console.WriteLine("คุณอยู่ในช่วงวัย: วัยผู้ใหญ่");
+    }
+    else if (age >= 51)
+    {
+        Console.WriteLine("คุณอยู่ในช่วงวัย: วัยชรา");
+    }
+    else
+    {
+        Console.WriteLine("อายุต้องมากกว่า 0");
+    }
+}
+else
+{
+    Console.WriteLine("กรุณากรอกตัวเลขที่ถูกต้อง");
+}
