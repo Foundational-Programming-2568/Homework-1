c70fe38ffd4950d57d41d32618b599822ed27882
Ratsamee246 <68030246@kmitl.ac.th>
2025-06-11 15:48:36 +0700

    full

---
+++ 68030246/project/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+Console.Write("กรุณาใส่อายุของคุณ: ");
+if (int.TryParse(Console.ReadLine(), out int age))
+{
+    if (age >= 1 && age <= 12)
+    {
+        Console.WriteLine("เด็ก");
+    }
+    else if (age >= 13 && age <= 19)
+    {
+        Console.WriteLine("วัยรุ่น");
+    }
+    else if (age >= 20 && age <= 50)
+    {
+        Console.WriteLine("วัยผู้ใหญ่");
+    }
+    else if (age >= 51)
+    {
+        Console.WriteLine("วัยชรา");
+    }
+    else
+    {
+        Console.WriteLine("กรุณาใส่อายุที่ถูกต้อง");
+    }
+}
+else
+{
+    Console.WriteLine("กรุณาใส่ตัวเลขที่ถูกต้อง");
+}
