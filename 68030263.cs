ca09ca5777db0ec692ed53ea1f0e88110271a867
Hashing0217 <89716712+Yunagrotohashing43556@users.noreply.github.com>
2025-06-11 15:19:54 +0700

    new

---
+++ 68030263.cs
@@ -0,0 +1,34 @@
+Console.WriteLine("เลขคี่ 1 - 100:");
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 2 != 0)
+        Console.Write($"{i} ");
+}
+Console.WriteLine("\n");
+
+// - แสดงเฉพาะเลขคู่
+Console.WriteLine("เลขคู่ 1 - 100:");
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 2 == 0)
+        Console.Write($"{i} ");
+}
+Console.WriteLine("\n");
+
+// - แสดงเฉพาะเลขจำนวนเฉพาะ
+bool IsPrime(int n)
+{
+    if (n < 2) return false;
+    for (int j = 2; j * j <= n; j++)
+        if (n % j == 0)
+            return false;
+    return true;
+}
+
+Console.WriteLine("เลขจำนวนเฉพาะ 1 - 100:");
+for (int i = 1; i <= 100; i++)
+{
+    if (IsPrime(i))
+        Console.Write($"{i} ");
+}
+Console.WriteLine();
