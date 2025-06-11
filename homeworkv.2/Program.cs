44150912831384086c79b064af455dd19b7c84a5
Mirqry <mirqunira@gmail.com>
2025-06-11 15:17:50 +0700

    complete

---
+++ homeworkv.2/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("กรุณากรอกอายุ: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int age))
+        {
+            string category;
+
+            if (age >= 1 && age <= 12)
+                category = "เด็ก";
+            else if (age >= 13 && age <= 19)
+                category = "วัยรุ่น";
+            else if (age >= 20 && age <= 50)
+                category = "วัยผู้ใหญ่";
+            else if (age >= 51)
+                category = "วัยชรา";
+            else
+                category = "อายุไม่ถูกต้อง";
+
+            Console.WriteLine($"คุณอยู่ในช่วง: {category}");
+        }
+        else
+        {
+            Console.WriteLine("กรุณากรอกตัวเลขที่ถูกต้อง");
+        }
+    }
+}
+
