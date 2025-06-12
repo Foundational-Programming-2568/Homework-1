97066b561aba6782c0ddf088407d05ad795c376e
theeradonpond <theeradonpond@gmail.com>
2025-06-12 08:24:57 +0700

    homework1

---
+++ Homework2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("กรุณากรอกอายุของคุณ: ");
+int age = int.Parse(Console.ReadLine() ?? "0");
+
+if (age >= 1 && age <= 12)
+{
+    Console.WriteLine("เด็ก");
+}
+else if (age >= 13 && age <= 19)
+{
+    Console.WriteLine("วัยรุ่น");
+}
+else if (age >= 20 && age <= 50)
+{
+    Console.WriteLine("วัยผู้ใหญ่");
+}
+else if (age >= 51)
+{
+    Console.WriteLine("วัยชรา");
+}
+else
+{
+    Console.WriteLine("กรุณากรอกอายุที่ถูกต้อง");
+}
